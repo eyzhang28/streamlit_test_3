e071fb3a5b484fab46cf70c73a4fe68da508d356
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-10 19:10:02 -0400

    Update streamlit_test3.py

--- streamlit_test3.py
+++ streamlit_test3.py
@@ -160,7 +160,7 @@ def compare_dict(df6, proofs_dictionary):
                 st.write('Different Trade Name')
 
 if st.button("Run Script"):
-     st.write("test")
+     st.write("test2")
      with open(r'\\199.221.111.170\qcom\QCEW Proof Review Team\ARS\Print Files\FY 22 - NVS 1st Mailing\DCCARS.YR22.NVS.F01.txt') as f:
           lines = f.readlines()
      st.write(lines)
